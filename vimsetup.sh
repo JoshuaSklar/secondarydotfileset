470aae9246b207191e42200fa3a98471e293e2b3
Josh Sklar <sklar@linux.vnet.ibm.com>
2015-04-22 15:37:31 -0700

    Renaming the pluginsetup to vimsetup
    Not any times I'm going to want just just my vimrc without the
    plugins. There are just interdepencies I don't really want to
    worry about abstracting out right now. Could change in future.

--- vimsetup.sh
+++ vimsetup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash 
 
+#Move vimrc file
+cp ./vimrc ~/.vimrc
+
 # Setup TPope's pathogen for managing Vim Plugins.
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Download and install solarized.
 git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized.git
+
+#Download and install fugitive, the great vim/git plugin
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive.git
+vim -u NONE "helptags vim-fugitive/doc" -c q
